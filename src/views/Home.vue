4d71c8c0b559450ef36e78fbdda37f30860f243f
Fantasy <charlesmaxwellyoung@gmail.com>
2019-12-31 09:43:15 +0800

    :test
    Test ci

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App，auto ci test"/>
   </div>
 </template>
 
